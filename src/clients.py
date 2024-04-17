35455e74eafe7f71a5750c536da242194fa6ed78
Guille-lg <71778504+Guille-lg@users.noreply.github.com>
2024-04-17 12:34:42 +0200

    feat: add total inferred candidates file and metrics notebook

--- src/clients.py
+++ src/clients.py
@@ -10,8 +10,11 @@ from openai import OpenAI
 import vertexai
 import langdetect
 from llamaapi import LlamaAPI
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
 from vertexai.preview.generative_models import GenerativeModel, ChatSession
 from vertexai.preview.language_models import TextGenerationModel
+from vertexai import generative_models
 from google.cloud import aiplatform_v1beta1
 import google.auth
 
@@ -104,7 +107,7 @@ class ChatGPTClient(Client):
                 [solucionar,concluir,determinar,decidir,arreglar]
 
             Asegúrate de que las alternativas estén encapsuladas dentro de corchetes cuadrados, separadas por comas y sin espacios entre las palabras. Esta última parte es muy importante.
-            No debes proporcionar ningún texto adicional. Tu respuesta debe ser SOLO el resultado esperado, el que se encuentra entre corchetes cuadrados. Las alternativas deben estar en español y ser sinónimos de la palabra compleja.
+            No debes proporcionar ningún texto adicional. Cada alternativa propuesta en la salida sólo puede ser de 1 palabra de longitud. Tu respuesta debe ser SOLO el resultado esperado, el que se encuentra entre corchetes cuadrados. Las alternativas deben estar en español y ser sinónimos de la palabra compleja.
 
         '''
         
@@ -139,6 +142,12 @@ class GeminiClient(Client):
         self.chat = self.model.start_chat(response_validation=False)
     
     def get_response(self, text,complex_word):
+        safety_config = {
+            generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_NONE,
+            generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_NONE,
+            generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_NONE,
+            generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_NONE,
+        }
         
         prompt = '''
                     
@@ -178,14 +187,15 @@ class GeminiClient(Client):
                 [solucionar,concluir,determinar,decidir,arreglar]
 
             Asegúrate de que las alternativas estén encapsuladas dentro de corchetes cuadrados, separadas por comas y sin espacios entre las palabras. Esta última parte es muy importante.
-            No debes proporcionar ningún texto adicional. Tu respuesta debe ser SOLO el resultado esperado, el que se encuentra entre corchetes cuadrados. Las alternativas deben estar en español y ser sinónimos de la palabra compleja.
+            No debes proporcionar ningún texto adicional. Cada alternativa propuesta en la salida sólo puede ser de 1 palabra de longitud. Tu respuesta debe ser SOLO el resultado esperado, el que se encuentra entre corchetes cuadrados. Las alternativas deben estar en español y ser sinónimos de la palabra compleja.
+            Quiero que siempre me devuelvas 5 alternativas, aunque no puedas encontrar sinónimos para la palabra compleja.
             A continuación, vas a recibir el texto y la palabra compleja que debes simplificar.
             
             ### TEXTO Y PALABRA COMPLEJAS A SIMPLIFICAR ###
         ''' + create_user_prompt(text,complex_word)
         
         try:
-            response = self.chat.send_message(prompt)
+            response = self.chat.send_message(prompt, safety_settings=safety_config)
             return response.text
         except Exception as e:
             return ''
@@ -239,7 +249,7 @@ class Palm2Client(Client):
                 [solucionar,concluir,determinar,decidir,arreglar]
 
             Asegúrate de que las alternativas estén encapsuladas dentro de corchetes cuadrados, separadas por comas y sin espacios entre las palabras. Esta última parte es muy importante.
-            No debes proporcionar ningún texto adicional. Tu respuesta debe ser SOLO el resultado esperado, el que se encuentra entre corchetes cuadrados. Las alternativas deben estar en español y ser sinónimos de la palabra compleja.
+            No debes proporcionar ningún texto adicional. Cada alternativa propuesta en la salida sólo puede ser de 1 palabra de longitud. Tu respuesta debe ser SOLO el resultado esperado, el que se encuentra entre corchetes cuadrados. Las alternativas deben estar en español y ser sinónimos de la palabra compleja.
             A continuación, vas a recibir el texto y la palabra compleja que debes simplificar.
             
             ### TEXTO Y PALABRA COMPLEJAS A SIMPLIFICAR ###
@@ -258,77 +268,64 @@ class Palm2Client(Client):
 
         return response.text
     
-class GemmaClient(Client):
+class MistralClient(Client):
     def __init__(self):
         super().__init__()
-        # Set the project ID
-        self.project_id = "word-simplification"
-
-        # Set the region
-        self.region = "us-central1"
-        self.endpoint_name = 'google_gemma-7b-it-1712831780451'
-
-        # The AI Platform services require regional API endpoints.
-        client_options = {"api_endpoint": f"{self.region}-aiplatform.googleapis.com"}
-        # Initialize client that will be used to create and send requests.
-        # This client only needs to be created once, and can be reused for multiple requests.
-        client = aiplatform_v1beta1.EndpointServiceClient(client_options=client_options)
-        endpoint = client.endpoint('google_gemma-7b-it-1712831780451')
-        deployed_model_id = endpoint.deployed_models[0].id
-        model_name = endpoint.deployed_models[0].model
-
-        # The format of the resources name is
-        # `projects/{project}/locations/{location}/models/{model}`
-        model_client = aiplatform_v1beta1.ModelServiceClient(client_options=client_options)
-        model = model_client.get_model(name=model_name)
+        self.client = MistralClient()
     
     def get_response(self, text, complex_word):
+        system = '''            
+            ### DESCRIPCIÓN DE LA TAREA ### 
 
-        # The AI Platform services require regional API endpoints.
-        client_options = {"api_endpoint": f"{self.region}-aiplatform.googleapis.com"}
-        # Initialize client that will be used to create and send requests.
-        # This client only needs to be created once, and can be reused for multiple requests.
-        client = aiplatform_v1beta1.PredictionServiceClient(client_options=client_options)
+            Quiero que actúes como un anotador de datos para una empresa de aprendizaje automático. Tu tarea es identificar una palabra compleja en una oración y proporcionar 5 alternativas más simples para esa palabra que harían el texto más fácil de leer.
+            Recuerda que el objetivo de estas anotaciones es hacer que el texto sea más accesible para personas con dificultades de comprensión lectora, por lo que tienes que tener en cuenta el contexto en el que se usa la palabra.
+            A continuación, te proporciono una serie de ejemplos para que aprendas lo que tienes que hacer. El input que te llegará será el texto a partir de la línea de entrada y lo que espero es que me devuelvas SOLO el output a partir de "salida", como en los siguientes ejemplos.
 
-        # The format of the endpoint resource name is
-        # `projects/{project}/locations/{location}/endpoints/{endpoint}`
-        endpoint = client.endpoint(self.endpoint_name)
+            ### EJEMPLOS ### 	
+            Ejemplo 1:
+            Prompt del usuario: 
+                Frase: El rápido zorro marrón salta sobre el perro perezoso.
+                Palabra compleja: salta
+            Salida del sistema:
+                [brinca,bota,rebota,avanza,retoza]
+        
+            Ejemplo 2:
+            Prompt del usuario: 
+                Frase: La tormenta causó un apagón en todo el vecindario.
+                Palabra compleja: apagón
+            Salida del sistema:
+                [corte,oscuridad,interrupción,fallo,apagado]
 
-        # The format of the instance schema uri is
-        # `gs://<your-gcs-bucket>/<import_export_path>/<schema_path>`
-        instance_schema_uri = "gs://YOUR_GCS_SOURCE_BUCKET/path_to_your_instance_schema_file"
+            Ejemplo 3:
+            Prompt del usuario: 
+                Frase: El niño encontró un tesoro enterrado en la playa.
+                Palabra compleja: enterrado
+            Salida del sistema:
+                [sepultado,oculto,soterrado,escondido,enterrado]
 
-        # The format of the parameters schema uri is
-        # `gs://<your-gcs-bucket>/<import_export_path>/<schema_path>`
-        parameters_schema_uri = "gs://YOUR_GCS_SOURCE_BUCKET/path_to_your_parameters_schema_file"
+            Ejemplo 4:
+            Prompt del usuario: 
+                Frase: El detective siguió las pistas hasta resolver el caso.
+                Palabra compleja: resolver
+            Salida del sistema:
+                [solucionar,concluir,determinar,decidir,arreglar]
 
-        # Read the instance from a file
-        with open("path/to/local/file.json", "rb") as f:
-            instance = json.load(f)
+            Asegúrate de que las alternativas estén encapsuladas dentro de corchetes cuadrados, separadas por comas y sin espacios entre las palabras. Esta última parte es muy importante.
+            No debes proporcionar ningún texto adicional. Cada alternativa propuesta en la salida sólo puede ser de 1 palabra de longitud. Tu respuesta debe ser SOLO el resultado esperado, el que se encuentra entre corchetes cuadrados. Las alternativas deben estar en español y ser sinónimos de la palabra compleja.
+    ''' 
 
-        # Set the parameters
-        parameters_dict = {}
-
-        # Set the instance and parameters
-        instances = [instance]
-        parameters = [parameters_dict]
-
-        # Perform the prediction request
-        response = client.predict(
-            endpoint=endpoint.name,
-            instances=instances,
-            parameters=parameters,
-            instance_schema_uri=instance_schema_uri,
-            parameters_schema_uri=parameters_schema_uri,
+        messages = [
+            ChatMessage(role='system', content=system),
+            ChatMessage(role='user', content=create_user_prompt(text, complex_word))
+        ]
+        
+        response = self.client.chat(
+            model = 'mistral-large-latest',
+            messages = messages
         )
-
-        print("response")
-        print(" deployed_model_id:", response.deployed_model_id)
-
-        # See gs://google-cloud-aiplatform/schema/predict/prediction/text_classification_1.0.0.yaml for the format of the predictions.
-        predictions = response.predictions
-        for prediction in predictions:
-            print(" prediction:", dict(prediction))
+        
+        return response.choices[0].message.content
+    
 
     
 class LLamaAPIClient(Client): # Client for mixtral-8x7b-instruct, llama-70b-chat, gemma-7b and falcon-40b-instruct
@@ -376,7 +373,7 @@ class LLamaAPIClient(Client): # Client for mixtral-8x7b-instruct, llama-70b-chat
                 [solucionar,concluir,determinar,decidir,arreglar]
 
             Asegúrate de que las alternativas estén encapsuladas dentro de corchetes cuadrados, separadas por comas y sin espacios entre las palabras. Esta última parte es muy importante.
-            No debes proporcionar ningún texto adicional. Tu respuesta debe ser SOLO el resultado esperado, el que se encuentra entre corchetes cuadrados. Las alternativas deben estar en español y ser sinónimos de la palabra compleja.
+            No debes proporcionar ningún texto adicional. Cada alternativa propuesta en la salida sólo puede ser de 1 palabra de longitud. Tu respuesta debe ser SOLO el resultado esperado, el que se encuentra entre corchetes cuadrados. Las alternativas deben estar en español y ser sinónimos de la palabra compleja.
             A continuación, vas a recibir el texto y la palabra compleja que debes simplificar.
         ''' 
         
